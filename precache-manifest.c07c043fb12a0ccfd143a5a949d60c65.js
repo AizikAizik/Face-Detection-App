e668d6ff6f0c000cd9fd61ebc113e9ac569d3a8a
Aizik_Anonymous <isaac.ogunleye@hotmail.com>
2020-05-16 10:01:05 +0100

    Updates

---
+++ precache-manifest.c07c043fb12a0ccfd143a5a949d60c65.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c023d3a58ce36c8b8393ed743c1c44e7",
+    "url": "/Face-Detection-App/index.html"
+  },
+  {
+    "revision": "6a54d7ce55be7eb7b27a",
+    "url": "/Face-Detection-App/static/css/2.3b6f1bda.chunk.css"
+  },
+  {
+    "revision": "039cf6f28d079fe2161a",
+    "url": "/Face-Detection-App/static/css/main.c4dd648e.chunk.css"
+  },
+  {
+    "revision": "6a54d7ce55be7eb7b27a",
+    "url": "/Face-Detection-App/static/js/2.d63972f5.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/Face-Detection-App/static/js/2.d63972f5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "039cf6f28d079fe2161a",
+    "url": "/Face-Detection-App/static/js/main.6772baa6.chunk.js"
+  },
+  {
+    "revision": "041c4854d9a8058b3ef4",
+    "url": "/Face-Detection-App/static/js/runtime-main.92dd9d58.js"
+  }
+]);
